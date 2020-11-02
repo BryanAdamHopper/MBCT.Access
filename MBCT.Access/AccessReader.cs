be7e3b73b298c64c8bcb10629da2f0638a517818
BryanAdamHopper <BryanAdamHopper@gmail.com>
2020-11-02 12:47:16 -0800

    Cleanup using section.

--- MBCT.Access/AccessReader.cs
+++ MBCT.Access/AccessReader.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Added Referance: System.Data; System.Windows.Forms;
 using System.Windows.Forms;
 using System.IO;
 using System.Data;
